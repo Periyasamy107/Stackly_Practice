0759e68b6ee3181c0f2bf0c1152f177db6698de2
Periyasamy <cperiyasamy085@gmail.com>
2026-07-06 13:29:54 +0530

    EB bill calculation changed based on commercial or domestic usage in EBBillProcessingSystem java file

--- JavaPractice/src/com/java/basic/EBBillProcessingSystem.java
+++ JavaPractice/src/com/java/basic/EBBillProcessingSystem.java
@@ -27,12 +27,20 @@ public class EBBillProcessingSystem {
          *  =======================================
          */
 
+        System.out.println("===============================");
+        System.out.println("   Please Enter The Details ");
+        System.out.println("===============================");
+        
         System.out.println();
         System.out.print("Enter your name      : ");
         String name = scanner.nextLine();
 
         System.out.print("Enter your EB number : ");
         int number = scanner.nextInt();
+        scanner.nextLine();
+        
+        System.out.print("Please either enter c or d : "); // c -> commercial use   d -> domestic use
+        String useType = scanner.nextLine();
 
         /** =======================================
          *  TAKE THE READINGS
@@ -63,14 +71,29 @@ public class EBBillProcessingSystem {
          *  =======================================
          */
         double ebBill = 0.0;
-
-        if (totalUnitsConsumed >= 101 && totalUnitsConsumed <= 200) {
-            ebBill = totalUnitsConsumed * 2.35;
-        } else if (totalUnitsConsumed >= 201 && totalUnitsConsumed <= 400) {
-            ebBill = (100 * 2.35) + ((totalUnitsConsumed - 100) * 4.70);
-        } else if (totalUnitsConsumed >= 401 && totalUnitsConsumed >= 500){
-            ebBill = (100 * 2.35) + (200 * 4.70) + ((totalUnitsConsumed - 300) * 6.30);
+        
+        if(useType.equals("d")) {
+        	if (totalUnitsConsumed >= 101 && totalUnitsConsumed <= 200) {
+                ebBill = totalUnitsConsumed * 2.35;
+            } else if (totalUnitsConsumed >= 201 && totalUnitsConsumed <= 400) {
+                ebBill = (100 * 2.35) + ((totalUnitsConsumed - 100) * 4.70);
+            } else if (totalUnitsConsumed >= 401 && totalUnitsConsumed >= 500){
+                ebBill = (100 * 2.35) + (200 * 4.70) + ((totalUnitsConsumed - 300) * 6.30);
+            }
+        } else if(useType.equals("c")){
+        	if (totalUnitsConsumed >= 101 && totalUnitsConsumed <= 200) {
+                ebBill = totalUnitsConsumed * 3.50;
+            } else if (totalUnitsConsumed >= 201 && totalUnitsConsumed <= 400) {
+                ebBill = (100 * 3.50) + ((totalUnitsConsumed - 100) * 6.25);
+            } else if (totalUnitsConsumed >= 401 && totalUnitsConsumed >= 500){
+                ebBill = (100 * 3.50) + (200 * 6.25) + ((totalUnitsConsumed - 300) * 8.50);
+            }
+        } else {
+        	System.out.println("Please enter c or d in order to process the eb bill.");
         }
+        
+
+        
 
 
         System.out.println();
@@ -89,6 +112,12 @@ public class EBBillProcessingSystem {
         System.out.println();
         System.out.println("EB Bill Processing:");
         System.out.println("--------------------------------------------------------------");
+        if(useType.equals("c")) {
+        	System.out.println("Consumption Type        : Commercial");
+        } else if (useType.equals("d")) {
+        	System.out.println("Consumption Type        : Domestic");
+        }
+        
         System.out.println("Total Units Consumed    : " + totalUnitsConsumed);
         System.out.println("Total Amount To Be Paid : Rs." +  Math.round(ebBill));
 
